f507d9bd769eda020fd4dac2c6f2dfe990c4f829
Adipat Larprattanakul <adipat.larprattanakul@thomsonreuters.com>
2016-02-19 13:41:04 +0700

    update script for deploy

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+/// <binding ProjectOpened='prepare' />
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['bower.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                metadata: '',
+                regExp: false
+            }
+        }
+    });
+
+    grunt.registerTask('default', 'Releasing to NPM', ['bump']);
+};
+
+
